cc94f87b22aee07e1e3f4ed9633375f1e8b54caa
igorbavand <igor.bvn@gmail.com>
2019-09-21 15:55:30 -0300

    ultima versao

--- silvio.py
+++ silvio.py
@@ -1,9 +1,8 @@
 from random import randint
 import os
 import time
-from pygame import mixer
+#from pygame import mixer
 import pygame
-
 
 def nauber():
     os.system('clear')
@@ -91,13 +90,11 @@ def nauber():
     time.sleep(0.1)
     print('                                  ,(((((((########(((((((////(((//**                        @')
     time.sleep(2)
-
     os.system('clear')
 
 
 def jogar():
     opc = 0
-
     while opc == 0:
         print()
         print()
@@ -118,7 +115,7 @@ def jogar():
         pygame.init()
         pygame.mixer.music.load('musica.ogg')
         pygame.mixer.music.play()
-        opc = input('		<-Precione enter para iniciar o jogo-> ')
+        opc = input('		<-Pressione enter para iniciar o jogo-> ')
     pygame.mixer.music.pause()
     ling = ['java', 'php', 'python', 'ruby', 'scala', 'lua', 'haskell', 'prolog', 'julia',
             'javascript', 'dart', 'kotlin', 'delphi', 'swift', 'bash', 'clojure', 'perl', 'sql']
@@ -126,6 +123,7 @@ def jogar():
               '_ ', '_ ', '_ ', '_ ', '_ ', '_ ', '_ ', '_ ', '_ ']
     veri = ['_ ', '_ ', '_ ', '_ ', '_ ', '_ ', '_ ', '_ ',
             '_ ', '_ ', '_ ', '_ ', '_ ', '_ ', '_ ', '_ ']
+    jogadas = []
     dica = 'Liguagens de programação'
     jogando = True
     letra = ''
@@ -155,6 +153,11 @@ def jogar():
             veri[i] = '_ '
         print()
         print()
+        print('		Tentativas: ',end='')
+        for bla in range(len(jogadas)):
+            print(jogadas[bla],' ',end='') 
+        print()
+        print()
         print('		Dica: {}'.format(dica))
         print()
         print('		Vidas: {}'.format(vidas))
@@ -163,32 +166,46 @@ def jogar():
     veri3 = list(veri3)
     while jogando:
         veri2 = ling[palavra]
-        print()
-        if veri3 == jogo:
-            print('		Parabéns você ganhou!!!')
-            pygame.init()
-            pygame.mixer.music.load('acerto.ogg')
-            pygame.mixer.music.play()
-            time.sleep(3)
-            os.system('clear')
-            jogar()
-        if vidas == 0:
-            print('		Perdeu!!! A palavra era: {}'.format(str(ling[palavra])))
-            pygame.init()
-            pygame.mixer.music.load('erro.ogg')
-            pygame.mixer.music.play()
-            time.sleep(3)
-            os.system('clear')
-            jogar()
-        letra = input('		Digite uma letra: ')
-        if not letra in str(ling[palavra]):
-            vidas -= 1
-        for k in range(len(ling[palavra])):
-            if letra == veri2[k]:
-                tracos[k] = letra+' '
-                veri3[k] = letra
-        tela()
-
+        repete = False
+        while repete == False:
+            print()
+            letra = input('		Digite uma letra: ')
+            if letra in jogadas:
+                
+                print()
+                print('		Letra já digitada, tente outra!')
+                time.sleep(1)
+                
+            if not letra in str(ling[palavra]):
+                if not letra in jogadas:
+                    vidas -= 1
+                if vidas <= 0:
+                    print()
+                    print('		Perdeu!!! A palavra era: {}'.format(str(ling[palavra])))
+                    pygame.init()
+                    pygame.mixer.music.load('erro.ogg')
+                    pygame.mixer.music.play()
+                    time.sleep(3)
+                    os.system('clear')
+                    jogar()
+                if not letra in jogadas:
+                    jogadas.append(letra)
+                    repete = True 
+                    
+            for k in range(len(ling[palavra])):
+                if letra == veri2[k]:
+                    tracos[k] = letra+' '
+                    veri3[k] = letra
+            tela()
+            if veri3 == jogo:
+                print()
+                print('		Parabéns você ganhou!!!')
+                pygame.init()
+                pygame.mixer.music.load('acerto.ogg')
+                pygame.mixer.music.play()
+                time.sleep(3)
+                os.system('clear')
+                jogar()
 
 nauber()
 jogar()
